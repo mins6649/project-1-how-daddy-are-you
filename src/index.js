aec154a98a98ae557b79f552f2d23507760bfce8
minyoungshin <mins6649@gmail.com>
2023-02-13 16:02:13 -0500

    rendering jokes in main body

--- src/index.js
+++ src/index.js
@@ -138,3 +138,57 @@ function appendScoreboard(scoreArray, element, imageArray = []) {
         element.appendChild(newRow)
     }
 }
+
+//CREATING THE JOKE LIST
+
+fetch(" http://localhost:3000/users")
+.then(res => res.json())
+.then(data => {
+    data.forEach(jokeObj =>{
+        renderJokes(jokeObj);
+    })
+})
+
+function renderJokes(jokeObj){
+    const aiContainer = document.getElementById("ai-punchline-container");
+    const userContainer = document.getElementById("user-punchline-container");
+    let prompt = document.getElementById("submitted-prompt");
+    let aiTitle = document.getElementById("ai-author");
+    let aiPunchline = document.getElementById("ai-punchline");
+    let aiNumOfLikes = document.getElementById("ai-punchline-likes");
+    let userTitle = document.getElementById("user-author")
+    let userPunchline = document.getElementById("user-punchline");
+    let userNumofLikes = document.getElementById("user-punchline-likes");
+
+    aiTitle.textContent = "The Daddy";
+    aiPunchline.textContent = jokeObj.cpuResponse;
+    aiNumOfLikes.textContent = jokeObj.cpuLikes;
+    userTitle.textContent = "User";
+    userPunchline.textContent = jokeObj.userResponse;
+    userNumofLikes.textContent = jokeObj.userLikes;
+
+    aiContainer.addEventListener("click", () =>{
+        jokeObj.cpuLikes++;
+        aiNumOfLikes.textContent = jokeObj.cpuLikes;
+        updateLikes(jokeObj);
+    })
+    userContainer.addEventListener("click", () =>{ 
+        jokeObj.userLikes++;
+        userNumofLikes.textContent = jokes.userLikes;
+        updateLikes(jokeObj);
+    }) 
+}
+
+
+//patch request for upddating likes
+function updateLikes(jokeObj){
+    fetch(`HTTP ${jokeObj.id}`,{
+        method: "PATCH",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(jokeObj)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
